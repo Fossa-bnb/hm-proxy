ca338a6e83c4a02706bc20a206129fd216a4595d
Harry Markley <tremarkley@gmail.com>
2018-03-06 17:19:50 -0800

    Configure proxy server to pass in id

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3004;
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
+app.use('/:id', express.static('public'));
 app.use('/scripts', express.static('node_modules'));
 app.listen(port, () => {
   console.log(`Server listening at port ${port}`);
